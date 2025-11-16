08025c2c399b1f5bff4223ebf857befaf552a97b
Avaxerrr <kevieralbarracin@hotmail.com>
2025-11-16 19:46:07 +0800

    Version 2.0-Build 1
    - Update gradle remove python dependency should work to all Jetbrain IDE

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+// QSS Toolkit version 1.1
+
 plugins {
   id("java")
   id("org.jetbrains.kotlin.jvm") version "2.1.0"
@@ -9,45 +11,54 @@ version = "1.1"
 
 repositories {
   mavenCentral()
+
   intellijPlatform {
     defaultRepositories()
   }
 }
 
-// Configure Gradle IntelliJ Plugin
+// Configure dependencies
 dependencies {
-  intellijPlatform {
-    // Change to PyCharm Community Edition
-    // The "PC" type indicates PyCharm Community Edition
-    create("PC", "2025.1")
-    testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
-
-    // For PyCharm Community, only use PythonCore (not Pythonid)
-    bundledPlugin("PythonCore")
-  }
+  // Coroutines support
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.3")
+
+  intellijPlatform {
+
+    intellijIdeaCommunity("2025.1")
+
+    // Test framework
+    testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+  }
 }
 
 intellijPlatform {
   pluginConfiguration {
     ideaVersion {
-      sinceBuild = "242"
+      // Support from 2025.1 (build 241) onwards
+      sinceBuild = "241"
+      // Allow until 2025.* versions
+      untilBuild = "251.*"
     }
 
     changeNotes = """
-            <h3>Version 1.1 (April 2025)</h3>
+            <h3>Version xx (not yet sure) - November 2025</h3>
             <ul>
-              <li><b>New Feature:</b> Create new QSS files directly from the IDE's "New" menu</li>
-              <li>Added file template for QSS files with default styling</li>
-              <li>Improved plugin integration with IDE's file creation workflow</li>
+                <li><b>Universal IDE Support:</b> Now works in ALL JetBrains IDEs (CLion, IntelliJ IDEA, PyCharm, WebStorm, etc.)</li>
+                <li><b>No Python Dependency:</b> Removed Python requirement - perfect for C++ Qt developers in CLion</li>
             </ul>
-            
+            <h3>Version 1.1 - April 2025</h3>
+            <ul>
+                <li><b>New Feature:</b> Create new QSS files directly from the IDE's New menu</li>
+                <li>Added file template for QSS files with default styling</li>
+                <li>Improved plugin integration with IDE's file creation workflow</li>
+            </ul>
             <h3>Version 1.0</h3>
             <ul>
-              <li>Initial release with basic QSS editing support</li>
-              <li>Syntax highlighting, code completion, and structure navigation</li>
-              <li>Color palette management and integrated color tools</li>
+                <li>Initial release with basic QSS editing support</li>
+                <li>Syntax highlighting, code completion, and structure navigation</li>
+                <li>Color palette management and integrated color tools</li>
             </ul>
         """.trimIndent()
   }
@@ -59,7 +70,13 @@ tasks {
     sourceCompatibility = "21"
     targetCompatibility = "21"
   }
+
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "21"
+  }
+
+  // Disable building searchable options (speeds up build)
+  buildSearchableOptions {
+    enabled = false
   }
 }
